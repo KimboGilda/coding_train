992036c81dac9dc708e5dd3d5c7714a14004dfb2
Nikos Bakogiannis <nikolaos.bakogiannis@crosswind.ch>
2022-10-11 10:04:01 +0200

    test post request

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const app = express();
 app.listen(3000, ()=> console.log('listening at 3000'));
 
 app.use(express.static('public'));
+
+// POST method route
+app.post('/api', (request, response) => {
+    console.log(request)
+  })
+
